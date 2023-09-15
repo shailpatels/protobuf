add454a4977bb67ad389bd062e6da67663246243
Shail Patel <shailpatel67@gmail.com>
2023-09-15 01:50:40 -0400

    use abseil through build.zig.zon

--- build.zig
+++ build.zig
@@ -19,27 +19,11 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_abseil = b.addStaticLibrary(.{
-        .name = "abseil",
+    const lib_proto = b.addStaticLibrary(.{
+        .name = "protobuf",
         .target = target,
         .optimize = optimize,
     });
-
-    lib_abseil.addIncludePath(.{ .path = "third_party/abseil-cpp" });
-    lib_abseil.linkLibCpp();
-
-    lib_abseil.addCSourceFiles(&cpp_files.absl_base_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_strings_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_log_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_hash_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_time_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_container_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_synchronization_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_status_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_numeric_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_crc_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_debugging_srcs, &(.{} ++ cpp_flags_all));
-    lib_abseil.addCSourceFiles(&cpp_files.absl_profiling_srcs, &(.{} ++ cpp_flags_all));
 
     const lib_utf8_validity = b.addStaticLibrary(.{
         .name = "utf8_validity",
@@ -47,34 +31,23 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const lib_abseil = b.dependency("abseil_cpp", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     lib_utf8_validity.linkLibC();
     lib_utf8_validity.addCSourceFiles(&.{"third_party/utf8_range/utf8_validity.cc"}, &.{});
-    lib_utf8_validity.addIncludePath(.{ .path = "third_party/utf8_range" });
-    lib_utf8_validity.addIncludePath(.{ .path = "third_party/abseil-cpp" });
-    lib_utf8_validity.linkLibrary(lib_abseil);
-
-    const lib_proto = b.addStaticLibrary(.{
-        .name = "protobuf",
-        .target = target,
-        .optimize = optimize,
-    });
+    lib_utf8_validity.linkLibrary(lib_abseil.artifact("abseil-cpp"));
+    lib_utf8_validity.installLibraryHeaders(lib_abseil.artifact("abseil-cpp"));
 
     lib_proto.addIncludePath(.{ .path = "src" });
-    lib_proto.addIncludePath(.{ .path = "third_party/abseil-cpp" });
     lib_proto.addIncludePath(.{ .path = "third_party/utf8_range" });
     lib_proto.linkLibCpp();
+    lib_proto.linkLibrary(lib_abseil.artifact("abseil-cpp"));
     lib_proto.linkLibrary(lib_utf8_validity);
-
+    lib_proto.installLibraryHeaders(lib_abseil.artifact("abseil-cpp"));
     lib_proto.addCSourceFiles(&cpp_files.libprotobuf_srcs, &(.{} ++ cpp_flags_all));
-
-    const lib_utf8_range = b.addStaticLibrary(.{
-        .name = "utf8_range",
-        .target = target,
-        .optimize = optimize,
-    });
-
-    lib_utf8_range.addCSourceFiles(&cpp_files.utf8_range_srcs, &.{});
-    lib_utf8_range.linkLibC();
 
     const lib_protoc = b.addStaticLibrary(.{
         .name = "protoc",
@@ -82,7 +55,6 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    lib_protoc.addIncludePath(.{ .path = "third_party/abseil-cpp" });
     lib_protoc.addIncludePath(.{ .path = "src" });
     lib_protoc.addCSourceFiles(&cpp_files.libprotoc_srcs, &(.{} ++ cpp_flags_all));
     lib_protoc.linkLibCpp();
@@ -97,7 +69,6 @@ pub fn build(b: *std.Build) void {
     protoc.linkLibrary(lib_proto);
     protoc.linkLibrary(lib_protoc);
     protoc.addIncludePath(.{ .path = "src" });
-    protoc.addIncludePath(.{ .path = "third_party/abseil-cpp" });
     protoc.addCSourceFiles(&.{
         "src/google/protobuf/compiler/main.cc",
     }, &(.{} ++ cpp_flags_all));
@@ -108,7 +79,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib_proto);
     b.installArtifact(lib_protoc);
     b.installArtifact(protoc);
-    b.installDirectory(.{
+    lib_proto.installHeadersDirectoryOptions(.{
         .source_dir = .{ .path = "src/google/protobuf" },
         .install_dir = .header,
         .install_subdir = "google/protobuf",
